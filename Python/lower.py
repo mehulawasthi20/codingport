4c2cace567dc51555137dcf676599830db3d60e7
mehulawasthi20 <mehulawasthi7@gmail.com>
2018-09-05 07:00:58 +0530

    Add files via upload

---
+++ Python/lower.py
@@ -0,0 +1,10 @@
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        
+        x = str.lower()
+        return x
+        
