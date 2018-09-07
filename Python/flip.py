df1aca846e41764ddae769cac8966eec4a989c3f
mehulawasthi20 <mehulawasthi7@gmail.com>
2018-09-07 21:33:23 +0530

    Leetcode problem 832

---
+++ Python/flip.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        Flipping an image
+        """
+        mylist = []
+        B = []
+
+        for item in A:
+	        mylist.append(item[::-1])
+	
+        for li in mylist:
+	        C=[]
+	        C = [1 if x == 0 else 0 for x in li]
+	        B.append(C)
+
+
+        return(B)
