85fd3635606832ee4e9b5dfaee3bd24d81a4465c
Mehul Awasthi <mehulawasthi7@gmail.com>
2018-09-18 08:21:57 +0530

    Solution for Leet Code problem 905

---
+++ Python/sortarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortArrayByParity(A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        
+        B = [] 
+        C = []
+        D = []
+        for element in A:
+            if element % 2 == 0:
+                B.append(element)
+            else:
+                C.append(element)
+        D = B + C
+        return D
+        
